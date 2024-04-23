a028548ba7c283a21b6ddb2f06d2b3b8f0d9b90f
ZhiziLi <97574482+ZhangBojin@users.noreply.github.com>
2024-04-23 12:53:40 +0800

    sqlsugar配置

---
+++ Models/userinfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace Models
+{
+    ///<summary>
+    ///
+    ///</summary>
+    [SugarTable("userinfo")]
+    public partial class userinfo
+    {
+           public userinfo(){
+
+
+           }
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           [SugarColumn(IsPrimaryKey=true,IsIdentity=true)]
+           public int id {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string uId {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public long phone {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:True
+           /// </summary>           
+           public string pwd {get;set;}
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using LoginServer.Middleware.Consul;
+using SqlSugar;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,11 +7,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<ConsulCenter>(); 
+builder.Services.AddSingleton<ConsulCenter>();
+
+builder.Services.AddSingleton<ISqlSugarClient>(s =>
+{
+    var sqlSugarScope = new SqlSugarScope(new ConnectionConfig()
+    {
+        DbType = SqlSugar.DbType.MySql,
+        ConnectionString = s.GetRequiredService<IConfiguration>().GetConnectionString("connectionString"),
+        IsAutoCloseConnection = true,
+    });
+    return sqlSugarScope;
+});
 
 var app = builder.Build();
 
 var task = app.Services.GetRequiredService<ConsulCenter>().ServiceRegistry();
+
+//app.Services.GetRequiredService<ISqlSugarClient>().DbFirst.IsCreateAttribute().CreateClassFile("E:\\Csharp\\LoginServer\\Models", "Models");
+
 
 if (app.Environment.IsDevelopment())
 {
