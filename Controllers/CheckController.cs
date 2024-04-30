f3b93d7cae20052b1f4bad135b0c33abc90b656c
ZhiziLi <97574482+ZhangBojin@users.noreply.github.com>
2024-04-30 14:11:59 +0800

    Revert "log测试"
    This reverts commit 7071dc32ca0475a553a74606481bb2aba738b969.

--- Controllers/CheckController.cs
+++ Controllers/CheckController.cs
@@ -4,12 +4,11 @@ namespace LoginServer.Controllers
 {
     [Route("Check")]
     [ApiController]
-    public class CheckController(ILogger<CheckController> logger) : ControllerBase
+    public class CheckController : ControllerBase
     {
         [HttpGet]
         public IActionResult Check()
         {
-            logger.LogInformation("心跳监测中...");
             return Ok();
         }
     }
